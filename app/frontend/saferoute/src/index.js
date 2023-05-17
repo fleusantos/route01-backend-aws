fcfa78c6705616fb93a51d347d31c169ff5a5d66
fleu <fleuguinersan@gmail.com>
2023-05-17 19:33:57 +0300

    s

--- app/frontend/saferoute/src/index.js
+++ app/frontend/saferoute/src/index.js
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './map'; 
+import Map from './map'; 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Map />
   </React.StrictMode>,
   document.getElementById('root')
 );
