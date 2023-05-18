9c79097166ed7a428c7efdca0687394476381961
fleu <fleuguinersan@gmail.com>
2023-05-18 04:17:22 +0300

    s

--- app/frontend/saferoute/src/App.js
+++ app/frontend/saferoute/src/App.js
@@ -1,23 +1,20 @@
-import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LoginPage from './LoginPage';
 import Map from './map';
 import About from './about';
 import UsedData from './used_data';
-import LoginPage from './LoginPage';
-
 
 function App() {
-    return (
-      <Router>
-          <Routes>
-            <Route path="/" element={<LoginPage />} />
-            <Route path="/map" element={<Map />} />
-            <Route path="/about" element={<About />} />
-            <Route path="/used_data" element={<UsedData />} />
-          </Routes>
-      </Router>
-    );
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<LoginPage />} />
+        <Route path="/map" element={<Map />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/used_data" element={<UsedData />} />
+      </Routes>
+    </Router>
+  );
 }
 
-
-export default App; 
+export default App;
