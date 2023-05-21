07639ffcaf435f49a5a3136cea94c22967d2730e
fleu <fleuguinersan@gmail.com>
2023-05-21 17:03:38 +0300

    s

--- app/frontend/saferoute/src/map.js
+++ app/frontend/saferoute/src/map.js
@@ -26,10 +26,10 @@ const MapComponent = () => {
         width: '100vw',
         height: '100vh'
       }}
-      center={{ lat: 48.7, lng: -74 }}
+      center={{ lat: 40.7, lng: -74 }}
       zoom={10}
-      disableDefaultUI={true}
       styles={mapStyle}
+      disableDefaultUI={true}
       onLoad={onLoad}
       onUnmount={onUnmount}
       restriction={{
