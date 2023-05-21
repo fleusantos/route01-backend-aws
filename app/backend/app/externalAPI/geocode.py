24e821d1f2ec803659245d915bf9e3d2ecb84c98
fleu <fleuguinersan@gmail.com>
2023-05-22 00:00:23 +0300

    swaped for .env key

--- app/backend/app/externalAPI/geocode.py
+++ app/backend/app/externalAPI/geocode.py
@@ -4,13 +4,21 @@ import aiohttp
 import asyncio
 from geocodio import GeocodioClient
 
+from dotenv import load_dotenv
+import os
+
 from app.backend.utility.coordinates import Point, Segment, Grid
 
 
 class GeocodioRequests:
-    key = "65b76561d765db5fd62522424dd5d443644f7fb"
     def __init__(self) -> None:
-        self.client = GeocodioClient(GeocodioRequests.key)
+        self.__key = self.__load_key()
+        self.client = GeocodioClient(self.__key)
+
+    def __load_key(self) -> str:
+        load_dotenv()
+        return os.getenv("GEOCODE_API_KEY")
+
 
     # def create_grid(self, seg:Segment, res_m=1000):
     #     res = res_m/1000/111.11
