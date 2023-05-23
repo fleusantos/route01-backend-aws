28cdc4b73493265360ea943fb86851b590ed4f36
fleu <fleuguinersan@gmail.com>
2023-05-23 22:37:42 +0300

    added predict_from_grid function

--- app/backend/app/internalAPI/map_responce.py
+++ app/backend/app/internalAPI/map_responce.py
@@ -5,26 +5,41 @@ import asyncio
 import tensorflow as tf
 from pandas import DataFrame
 
-from app.backend.utility.coordinates import Point, Segment, Grid
+from app.backend.utility.coordinates import Grid
 
 class mapResponce:
-    def __init__(self, model_path="../../ml/model/model.h5") -> None:
+    def __init__(self, model_path="./app/backend/app/ml/model/model.h5") -> None:
         self._model = self.__load_model(model_path)
-
 
     def __load_model(self, path) -> tf.saved_model:
         model = tf.saved_model.load(path)
         return model
     
-    def predict_from_data(self, input_data:dict) -> list:
+    def predict_from_grid(self, grid:Grid) -> Grid:
         try:
-            if set(input_data.keys()) != ('pop_count_adj', 'income'):
-                raise ValueError(f"Incorrect keys for input_data({input_data.keys()}), should be 'pop_count_adj', 'income'")   
+            values = {'pop_count_adj': [], 'income': []}
+            for c in grid.chunks:
+                values['pop_count_adj'].append(c.data['pop_count_adj']) 
+                values['income'].append(c.data['income']) 
+                
+            prediction = self.predict_from_data(values)
+
+            for i in len(grid.chunks):
+                grid.chunks[i].data['crime_level'] = prediction[i]
+
+        except Exception as e:
+            raise ValueError(f"Grid prediction error: \n{e}")
+    
+    def predict_from_data(self, input_data: dict) -> list:
+        try:
+            if not set(input_data.keys()) >= {'pop_count_adj', 'income'}:
+                raise ValueError(f"Keys not found for input_data({input_data.keys()}), should contain 'pop_count_adj', 'income'")
+            
             values = DataFrame(input_data, index=[0])
             res = []
-            for k, v in values.items():
-                res.append(self._model.predict(values))
+            for _, v in values.itertuples():
+                res.append(self._model.predict(v))
             return res
-        
+
         except Exception as e:
             raise ValueError(f"Prediction error: \n{e}")
