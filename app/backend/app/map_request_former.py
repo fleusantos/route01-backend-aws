6f10c3539d87ca036913a283919b314fbe3d4cdf
fleu <fleuguinersan@gmail.com>
2023-05-29 01:46:19 +0300

    updated load_from_worldpop

--- app/backend/app/map_request_former.py
+++ app/backend/app/map_request_former.py
@@ -7,8 +7,9 @@ from pandas import DataFrame
 
 from app.backend.utility.coordinates import Grid
 from app.backend.app.externalAPI.geocode import GeocodioRequests
+from app.backend.app.externalAPI.worldpop import load_pops_from_file
 
-#координати -> перевірка чи кешовано -> якшо ні, то створити грід -> заповнити його через worldpop і geocode -> кинути  цей грід в map_response
+#координати -> перевірка чи кешовано -> якшо ні, то створити грід -> заповнити його через worldpop і geocode -> нормалізувати дату в ньому -> кинути грід в map_response
 
 async def load_from_geocode(grid:Grid) -> bool:
     gr = GeocodioRequests()
@@ -16,5 +17,5 @@ async def load_from_geocode(grid:Grid) -> bool:
     return True
 
 async def load_from_worldpop(grid:Grid) -> bool:
-    
+    await load_pops_from_file(grid)
     return True
