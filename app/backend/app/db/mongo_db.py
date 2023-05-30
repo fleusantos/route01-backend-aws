270713283016749f847a23825725e6a5f138c158
fleu <fleuguinersan@gmail.com>
2023-05-31 00:35:05 +0300

    updated post_grid func

--- app/backend/app/db/mongo_db.py
+++ app/backend/app/db/mongo_db.py
@@ -4,7 +4,7 @@ from pymongo.server_api import ServerApi
 from dotenv import load_dotenv
 import os
 
-from app.backend.utility.coordinates import Point, Segment, Grid
+from app.backend.utility.coordinates import Point, Segment, Grid, create_grid
 
 class Mongo:
     def __init__(self) -> None:
@@ -19,15 +19,28 @@ class Mongo:
     async def post_grid(self, grid:Grid):
         data = await grid.to_json()
         for item in data:
-            query = { 'cords': { 'vert': item['cords']['vert'] } }
+            query = {'cords': {'vert': item['cords']['vert']}}
             existing_doc = await self.mapdb.find_one(query)
+            
             if existing_doc is None:
                 await self.mapdb.insert_one(item)
+            elif (existing_doc['data']['pop_count_adj'] == -1
+                    or existing_doc['data']['income'] == -1
+                    or existing_doc['data']['crime_level'] == -1
+                    ):
+                update_query = {
+                    '$set': {
+                        'data.pop_count_adj': existing_doc['data']['pop_count_adj'] if existing_doc['data']['pop_count_adj'] == -1 else item['data']['pop_count_adj'],
+                        'data.income': existing_doc['data']['income'] if existing_doc['data']['income'] == -1 else item['data']['income'],
+                        'data.crime_level': existing_doc['data']['crime_level'] if existing_doc['data']['crime_level'] == -1 else item['data']['crime_level']
+                    }
+                }
+                await self.mapdb.update_one({'id': existing_doc['id']}, update_query)
 
     def test(self):
         try:
             self.client.admin.command('ping')
-            print("Pinged your deployment. You successfully connected to MongoDB!")
+            print("MongoDB connection succsesful!")
         except Exception as e:
             print(e)
 
