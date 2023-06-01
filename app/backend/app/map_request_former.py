e375d1e7386f05fa2266d054023dc5c47db3d58b
fleu <fleuguinersan@gmail.com>
2023-06-01 03:50:00 +0300

    added load_from_model

--- app/backend/app/map_request_former.py
+++ app/backend/app/map_request_former.py
@@ -8,6 +8,7 @@ from pandas import DataFrame
 from app.backend.utility.coordinates import Grid
 from app.backend.app.externalAPI.geocode import GeocodioRequests
 from app.backend.app.externalAPI.worldpop import load_pops_from_file
+from app.backend.app.internalAPI.map_responce import mapResponce
 
 #координати -> перевірка чи кешовано -> якшо ні, то створити грід -> заповнити його через worldpop і geocode -> доповнтити пустуючі значення-> нормалізувати дату в ньому -> кинути грід в map_response
 
@@ -18,4 +19,9 @@ async def load_from_geocode(grid:Grid) -> bool:
 
 async def load_from_worldpop(grid:Grid) -> bool:
     await load_pops_from_file(grid)
+    return True
+
+async def load_from_model(grid:Grid) -> bool:
+    mr = mapResponce()
+    await mr.predict_from_grid(grid)
     return True
