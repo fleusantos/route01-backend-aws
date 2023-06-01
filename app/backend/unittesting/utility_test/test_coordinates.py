579f2ec6d181540111118cf1d906571e2897b1d1
fleu <fleuguinersan@gmail.com>
2023-06-01 03:51:09 +0300

    updated tests

--- app/backend/unittesting/utility_test/test_coordinates.py
+++ app/backend/unittesting/utility_test/test_coordinates.py
@@ -1,39 +1,46 @@
 import pytest
-from app.backend.utility.coordinates import Point, Segment, Grid, create_grid
+
+from app.backend.utility.coordinates import Segment, Grid, Point, create_grid
+
 
 @pytest.fixture
 def sample_segment():
-    points = [Point(0, 0), Point(0, 1), Point(1, 1), Point(1, 0)]
-    return Segment(points)
+    points = [
+        Point(0, 0),
+        Point(1, 0),
+        Point(1, 1),
+        Point(0, 1)
+    ]
+    return Segment(points=points)
 
-def test_segment_get_center(sample_segment):
-    assert sample_segment.center.x == 0.5
-    assert sample_segment.center.y == 0.5
 
-def test_segment_point_in_segment(sample_segment):
-    point_inside = Point(0.5, 0.5)
-    point_outside = Point(2, 2)
+@pytest.fixture
+def sample_grid(sample_segment):
+    return create_grid(sample_segment, 100)
 
-    assert sample_segment.point_in_segment(point_inside) is True
-    assert sample_segment.point_in_segment(point_outside) is False
 
-def test_grid_split_by_res(sample_segment):
-    grid = Grid(sample_segment)
-    chunks = grid.split_by_res(0.1)
+def test_remove_missing_values(sample_grid):
+    # Test removing missing values
+    # Set some data values to -1
+    sample_grid.chunks[0].data['pop_count_adj'] = -1
+    sample_grid.chunks[1].data['income'] = -1
 
-    assert len(chunks) == 4
+    # Call the function to remove missing values
+    sample_grid.remove_missing_values()
 
-def test_grid_get_centers(sample_segment):
-    grid = Grid(sample_segment)
-    grid.split_by_res(0.1)
-    centers = grid.get_centers()
+    # Check if the missing values have been replaced
+    assert sample_grid.chunks[0].data['pop_count_adj'] != -1
+    assert sample_grid.chunks[1].data['income'] != -1
 
-    assert len(centers) == 4
 
-def test_create_grid():
-    seg = Segment([Point(0, 0), Point(0, 1), Point(1, 1), Point(1, 0)])
-    grid = create_grid(seg, res_m=1000)
-    
-    assert isinstance(grid, Grid)
-    assert len(grid.chunks) > 0
-    assert grid.resolution == 0.001
+def test_normalize_data(sample_grid):
+    # Set some sample data values
+    sample_grid.chunks[0].data['pop_count_adj'] = 100
+    sample_grid.chunks[1].data['income'] = 500
+
+    # Call the function to normalize data
+    sample_grid.normalize_data()
+
+    # Check if the data values have been normalized
+    assert sample_grid.chunks[0].data['pop_count_adj'] == 0
+    assert sample_grid.chunks[1].data['income'] == 0.5
