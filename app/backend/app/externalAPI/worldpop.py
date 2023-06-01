d03283f99e91aaea1c0889113785cc8655cc866e
fleu <fleuguinersan@gmail.com>
2023-06-01 19:20:25 +0300

    fixed reulting values

--- app/backend/app/externalAPI/worldpop.py
+++ app/backend/app/externalAPI/worldpop.py
@@ -26,17 +26,28 @@ class WorldPopRequests:
 async def _get_distance(point1, point2):
     x1, y1 = point1
     x2, y2 = point2
-
+    # print(f'x1:{x1}, x2:{x2}, y1:{y1}, y2:{y2}, point1{point1}, point2{point2}')
+    # print(f'x2-x1:{(x2 - x1)}, y2-y1:{(y2 - y1)}, sqx:{(x2 - x1) ** 2}, sqy:{(y2 - y1) ** 2}, math.sqrt:{math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)}\n')
     distance = math.sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
     return distance
 
 
 async def _calculate_average(cells, point:Point, resolution):
     result = 0
+    # print(f'cell len:{len(cells)}')
+    # print(f'{cells}\npoint{point}\n')
     for data, cords in cells.items():
+        if data<0:
+            data = 0
+        
         distance = await _get_distance(cords, [point.x, point.y])
-        result += data * (distance / resolution) / len(cells) #TODO check if x and y not swapped places
-    
+        # print(f'{distance}')
+        # print(f'cords:{cords}, points:{[point.x, point.y]}, data:{data}')
+        result += data * (distance / resolution) / len(cells)
+        # print(f'distance:{distance}, dis/res:{(distance / resolution)}, /len:{data * (distance / resolution)}, res+={data * (distance / resolution) / len(cells)}\n')
+    # print(f'result:{result}\n')
+    # print('-'*40)
+    # print('\n')
     return result
 
 async def async_range(stop, start=0, step=1):
@@ -52,20 +63,21 @@ async def load_pops_from_file(grid:Grid):
     try:
         with rasterio.open("pop_data_small.tif") as src:
             image = src.read(1)
-            bounds = src.bounds
-            resolution = (src.transform.a + src.transform.e) / 2
+            bounds = (-90, 30, -68, 44)
+            resolution = (src.res[0] + src.res[1]) / 2
             step = (abs(bounds[2] - bounds[0])/np.shape(image)[0] + abs(bounds[3] - bounds[1])/np.shape(image)[1]) / 2
 
         tasks = []
+        # print(resolution)
         async for i in async_range(len(grid.chunks)):
             top_left_x = int(abs(grid.chunks[i].center.x - bounds[0])//step) # top_left index
             top_left_y = int(abs(grid.chunks[i].center.y - bounds[3])//step)  # top_left index
             # print(f'tlx:{top_left_x}, tly:{top_left_y}')
             task = asyncio.create_task(_calculate_average({
-                image[top_left_x, top_left_y]: [bounds[0] + top_left_x * step, bounds[3] + top_left_y * step],
-                image[top_left_x + 1, top_left_y]: [bounds[0] + (top_left_x + 1) * step, bounds[3] + top_left_y * step], 
-                image[top_left_x + 1, top_left_y + 1]: [bounds[0] + (top_left_x + 1) * step, bounds[3] + (top_left_y + 1) * step], 
-                image[top_left_x, top_left_y + 1]: [bounds[0] + top_left_x * step, bounds[3] + (top_left_y + 1) * step]
+                image[top_left_x, top_left_y]: [bounds[0] + top_left_x * step, bounds[3] - top_left_y * step],
+                image[top_left_x + 1, top_left_y]: [bounds[0] + (top_left_x + 1) * step, bounds[3] - top_left_y * step], 
+                image[top_left_x + 1, top_left_y + 1]: [bounds[0] + (top_left_x + 1) * step, bounds[3] - (top_left_y + 1) * step], 
+                image[top_left_x, top_left_y + 1]: [bounds[0] + top_left_x * step, bounds[3] - (top_left_y + 1) * step]
             }, grid.chunks[i].center, resolution))
 
             tasks.append(task)
