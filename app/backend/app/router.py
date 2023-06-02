cbca5d1e4ec91e1e3bc6389836363f4e3aad3b2c
fleu <fleuguinersan@gmail.com>
2023-06-02 23:46:09 +0300

    added routes

--- app/backend/app/router.py
+++ app/backend/app/router.py
@@ -1,11 +1,22 @@
+from app.backend.app.db.mongo_db import Mongo
+from app.backend.utility.bounds import Bounds
+
 from fastapi import APIRouter
 
-router = APIRouter()
-# have to finish
-@router.get("/items")
-async def get_items():
-    return {"message": "Get all items"}
 
-@router.get("/items/{item_id}")
-async def get_item(item_id: int):
-    return {"message": f"Get item with ID {item_id}"}
+router = APIRouter()
+mongo_client = None
+
+@router.on_event("startup")
+async def startup_event():
+    global mongo_client
+    mongo_client = Mongo()
+    await mongo_client.test()
+
+@router.get("/ping")
+async def ping():
+    return {"Success": True}
+
+@router.get("/db/{bounds}")
+async def get_item(bounds: Bounds):
+    return await mongo_client.get_in_bounds(bounds.bounds)
