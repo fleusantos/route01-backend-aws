763a6edef0760e9e5f8718fe824d9308b048fdb7
fleu <fleuguinersan@gmail.com>
2023-06-03 00:11:14 +0300

    changed get_data_from_bounds and added docstring

--- app/backend/app/router.py
+++ app/backend/app/router.py
@@ -1,8 +1,7 @@
 from app.backend.app.db.mongo_db import Mongo
-from app.backend.utility.bounds import Bounds
+from fastapi import APIRouter, HTTPException
 
-from fastapi import APIRouter
-
+import json
 
 router = APIRouter()
 mongo_client = None
@@ -13,10 +12,32 @@ async def startup_event():
     mongo_client = Mongo()
     await mongo_client.test()
 
-@router.get("/ping")
+@router.get("/")
 async def ping():
     return {"Success": True}
 
-@router.get("/db/{bounds}")
-async def get_item(bounds: Bounds):
-    return await mongo_client.get_in_bounds(bounds.bounds)
+@router.get("/db/get_data_from_bounds=l:{l},b:{b},r:{r},t:{t}")
+async def get_data_from_bounds(l: float, b: float, r: float, t: float):
+    """
+    Retrieve map data within the specified bounds.
+
+    Arguments:
+    - l (float): The left boundary value.
+    - b (float): The bottom boundary value.
+    - r (float): The right boundary value.
+    - t (float): The top boundary value.
+
+    Returns:
+    - json: A json containing the result of the query.
+
+    Raises:
+    - HTTPException(400): If the bounds are invalid.
+    """
+    bounds = (l, b, r, t)
+    if (len(bounds) != 4 or
+        bounds[2] - bounds[0] < 0 or 
+        bounds[3] - bounds[1] < 0
+        ):
+        raise HTTPException(status_code=400, detail="Invalid bounds")
+    res = await mongo_client.get_in_bounds(bounds)
+    return json.dumps(res)
